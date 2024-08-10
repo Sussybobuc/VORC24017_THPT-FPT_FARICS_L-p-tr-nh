bfbdfc6f91a5cc8aa5f06b409a2316a7e11cc834
Sussybobuc <itwasajokestop@gmail.com>
2024-08-10 12:07:51 +0700

    Update PSPcontroller.h

--- PSPcontroller.h
+++ PSPcontroller.h
@@ -1,7 +1,7 @@
 #ifndef PSP_CONTROLLER_H
 #define PSP_CONTROLLER_H
 
-#include <PS2X_lib.h>
+#include <PS2X_lib.h>  // Chắc chắn rằng thư viện PS2X_lib được bao gồm đúng cách
 
 // Khai báo đối tượng PS2X
 extern PS2X ps2x;
