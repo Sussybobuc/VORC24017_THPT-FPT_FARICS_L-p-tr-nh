8b0c447a1ca8eb0a545c3e893916ef5f7c754f1f
Sussybobuc <itwasajokestop@gmail.com>
2024-08-10 11:40:33 +0700

    Delete Library.cpp

--- Library.cpp
+++
@@ -1,4 +0,0 @@
-#include <Wire.h> // Thư viện I2C của Arduino
-#include <Adafruit_PWMServoDriver.h> // Thư viện PCA9685
-#include <Adafruit_TCS34725.h> // Thư viện cảm biến màu TCS34725
-#include <PS2X_lib.h> // Thư viện PS2 cho tay cầm điều khiển
