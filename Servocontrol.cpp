3e2923f670b3b5c036a2d7a2cbca9e3f3c91817a
Sussybobuc <itwasajokestop@gmail.com>
2024-08-11 09:29:53 +0700

    Update Servocontrol.cpp

--- Servocontrol.cpp
+++ Servocontrol.cpp
@@ -1,28 +1,28 @@
 #include "ServoControl.h"
-#include <Arduino.h>
-#include "MotorControl.h"
+#include <Adafruit_PWMServoDriver.h>
 
-bool initializeServoControl() {
-    // Đã được khởi tạo trong initializeMotorControl()
-    return true;
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+void initializeServo180() {
+    pwm.begin();
+    pwm.setOscillatorFrequency(27000000);
+    pwm.setPWMFreq(50);
+    Wire.setClock(400000);
 }
 
-bool setServoAngle(uint8_t servo, uint16_t angle) {
-    if (servo >= 16 || angle > 180) {
-        Serial.println("Invalid servo or angle value!");
-        return false;
-    }
-    uint16_t pwm_value = map(angle, 0, 180, 102, 512);
-    pwm.setPWM(servo, 0, pwm_value);
-    return true;
+void initializeServo360() {
+    pwm.begin();
+    pwm.setOscillatorFrequency(27000000);
+    pwm.setPWMFreq(50);
+    Wire.setClock(400000);
 }
 
-bool setServoMicroseconds(uint8_t servo, uint16_t microseconds) {
-    if (servo >= 16 || microseconds < 500 || microseconds > 2500) {
-        Serial.println("Invalid servo or microseconds value!");
-        return false;
-    }
-    uint16_t pwm_value = map(microseconds, 500, 2500, 102, 512);
-    pwm.setPWM(servo, 0, pwm_value);
-    return true;
+void setServoAngle180(int servoIndex, int angle) {
+    int pwmValue = map(angle, 0, 180, 102, 512);
+    pwm.setPWM(servoIndex, 0, pwmValue);
+}
+
+void setServoSpeed360(int servoIndex, int speed) {
+    int pwmValue = map(speed, -255, 255, 102, 512);
+    pwm.setPWM(servoIndex, 0, pwmValue);
 }
